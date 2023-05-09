2e7d6d39750bc589479b100cafa7661593118c92
SyncedTogether <xdskyward@gmail.com>
2023-05-08 19:57:50 -0700

    meta data display, searching, and playlist downloading

--- SC/MainWindow.xaml.cs
+++ SC/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using SoundCloudExplode;
 using SC.Properties;
+using SoundCloudExplode.Search;
+using System.Linq;
 
 namespace SC
 {
@@ -31,8 +33,8 @@ namespace SC
         private async void DownloadButton_Click(object sender, RoutedEventArgs e)
         {
             // Get the track link and download path from the text boxes
-            var trackLink = TrackLinkTextBox.Text;
-            var downloadPath = DestinationTextBox.Text;
+            string trackLink = TrackLinkTextBox.Text;
+            string downloadPath = DestinationTextBox.Text;
 
             // Validate the track link and download path
             if (string.IsNullOrWhiteSpace(trackLink) || !Uri.TryCreate(trackLink, UriKind.Absolute, out Uri uri) || !uri.Host.Equals("soundcloud.com"))
@@ -41,8 +43,8 @@ namespace SC
                 return;
             }
 
-            var soundcloud = new SoundCloudClient();
-            var track = await soundcloud.Tracks.GetAsync(trackLink);
+            SoundCloudClient soundcloud = new SoundCloudClient();
+            SoundCloudExplode.Track.TrackInformation track = await soundcloud.Tracks.GetAsync(trackLink);
 
             if (string.IsNullOrWhiteSpace(downloadPath))
             {
@@ -50,14 +52,18 @@ namespace SC
                 return;
             }
 
-            var trackName = string.Join("_", track.Title.Split(System.IO.Path.GetInvalidFileNameChars()));
+            string trackName = string.Join("_", track.Title.Split(System.IO.Path.GetInvalidFileNameChars()));
             downloadPath = System.IO.Path.Combine(downloadPath, $"{trackName}.mp3");
 
-            var progress = new Progress<double>(value =>
+
+
+            Progress<double> progress = new Progress<double>(value =>
             {
                 DownloadProgressBar.Value = value;
             });
 
+
+            UpdateMetaDataDisplay(track);
             await soundcloud.DownloadAsync(track, downloadPath, progress);
 
             // Download complete, show a message and reset the progress bar
@@ -75,5 +81,108 @@ namespace SC
             Settings.Default.LastDownloadPath = DestinationTextBox.Text;
             Settings.Default.Save();
         }
+
+        private async void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string searchQuery = SearchBox.Text;
+
+            if (string.IsNullOrWhiteSpace(searchQuery))
+            {
+                MessageBox.Show("Search query cannot be empty");
+                return;
+            }
+
+            SoundCloudClient soundcloud = new SoundCloudClient();
+            List<ISearchResult> results = await soundcloud.Search.GetResultsAsync(searchQuery);
+
+            // Only show the first 10 tracks in the results
+            SearchResultsBox.ItemsSource = results.OfType<TrackSearchResult>().Take(10);
+        }
+
+        private async void DownloadPlaylistButton_Click(object sender, RoutedEventArgs e)
+        {
+            string playlistLink = PlaylistLinkTextBox.Text;
+
+            if (string.IsNullOrWhiteSpace(playlistLink) || !Uri.TryCreate(playlistLink, UriKind.Absolute, out Uri uri) || !uri.Host.Equals("soundcloud.com"))
+            {
+                MessageBox.Show("Invalid playlist link");
+                return;
+            }
+
+            string downloadPath = DestinationTextBox.Text;
+
+            if (string.IsNullOrWhiteSpace(downloadPath))
+            {
+                MessageBox.Show("Invalid download path");
+                return;
+            }
+
+            SoundCloudClient soundcloud = new SoundCloudClient();
+            SoundCloudExplode.Playlist.PlaylistInformation playlist = await soundcloud.Playlists.GetAsync(playlistLink);
+
+            // Download all tracks in the playlist
+            foreach (SoundCloudExplode.Track.TrackInformation track in playlist.Tracks)
+            {
+                UpdateMetaDataDisplay(track);
+                string trackName = string.Join("_", track.Title.Split(System.IO.Path.GetInvalidFileNameChars()));
+                string trackDownloadPath = System.IO.Path.Combine(downloadPath, $"{trackName}.mp3");
+                Progress<double> progress = new Progress<double>(value =>
+                {
+                    DownloadProgressBar.Value = value;
+                });
+                await soundcloud.DownloadAsync(track, trackDownloadPath, progress);
+            }
+
+            MessageBox.Show("Playlist download complete!");
+        }
+
+        private void UpdateMetaDataDisplay(SoundCloudExplode.Track.TrackInformation track)
+        {
+            MetaDataArtwork.Source = new BitmapImage(track.ArtworkUrl);
+            MetaDataTitle.Text = $"Title: {track.Title}";
+            MetaDataGenre.Text = $"Genre: {track.Genre}";
+            MetaDataDuration.Text = $"Duration: {track.Duration} seconds";
+            MetaDataDisplayDate.Text = $"Display Date: {track.DisplayDate}";
+            MetaDataCaption.Text = $"Caption: {track.Caption}";
+            MetaDataDescription.Text = $"Description: {track.Description}";
+        }
+
+        private async void SearchResultsBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (SearchResultsBox.SelectedItem is TrackSearchResult selectedTrack)
+            {
+                string downloadPath = DestinationTextBox.Text;
+
+                if (string.IsNullOrWhiteSpace(downloadPath))
+                {
+                    MessageBox.Show("Invalid download path");
+                    return;
+                }
+
+                SoundCloudClient soundcloud = new SoundCloudClient();
+                SoundCloudExplode.Track.TrackInformation track = await soundcloud.Tracks.GetAsync(selectedTrack.Uri.ToString());
+
+                string trackName = string.Join("_", track.Title.Split(System.IO.Path.GetInvalidFileNameChars()));
+                downloadPath = System.IO.Path.Combine(downloadPath, $"{trackName}.mp3");
+
+                UpdateMetaDataDisplay(track);
+
+                Progress<double> progress = new Progress<double>(value =>
+                {
+                    DownloadProgressBar.Value = value;
+                });
+
+                await soundcloud.DownloadAsync(track, downloadPath, progress);
+
+                // Download complete, show a message and reset the progress bar
+                MessageBox.Show("Download complete!");
+                DownloadProgressBar.Value = 0;
+
+                // Save the download path to settings
+                Properties.Settings.Default.LastDownloadPath = downloadPath;
+                Properties.Settings.Default.Save();
+            }
+        }
+
     }
 }
